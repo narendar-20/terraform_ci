1281c130960461e0ea5cbc73bf69bb826cf17584
Ramesh <116292847+RameshDumala1@users.noreply.github.com>
2025-05-06 11:58:00 +0530

    Add files via upload

---
+++ ansible/inventory.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import json
+
+def main():
+    with open("frontend_ip.txt") as f:
+        frontend_ip = f.read().strip()
+    with open("backend_ip.txt") as f:
+        backend_ip = f.read().strip()
+
+    inventory = {
+        "frontend": {
+            "hosts": [frontend_ip],
+        },
+        "backend": {
+            "hosts": [backend_ip],
+        },
+        "_meta": {
+            "hostvars": {
+                frontend_ip: {"ansible_user": "ec2-user"},
+                backend_ip: {"ansible_user": "ubuntu"},
+            }
+        }
+    }
+    print(json.dumps(inventory, indent=2))
+
+if __name__ == "__main__":
+    main()
