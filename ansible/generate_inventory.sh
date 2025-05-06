b51e0cf39100aeafe062c3afa522e99165384af9
Ramesh <116292847+RameshDumala1@users.noreply.github.com>
2025-05-06 16:46:50 +0530

    Update generate_inventory.sh

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# Fix permission just in case
-chown jenkins:jenkins inventory.ini 2>/dev/null
-rm -f inventory.ini  # remove old one to avoid permissions issue
+FRONTEND_IP=$(terraform -chdir=../terraform output -raw frontend_ip)
+BACKEND_IP=$(terraform -chdir=../terraform output -raw backend_ip)
 
-# Load IPs
-BACKEND_IP=$(cat backend_ip.txt)
-FRONTEND_IP=$(cat frontend_ip.txt)
-
-# Create clean inventory.ini
 cat <<EOF > inventory.ini
-[backend]
-$BACKEND_IP ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/jenkins_key
-
 [frontend]
-$FRONTEND_IP ansible_user=ec2-user ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/jenkins_key
-EOF
+c8.local ansible_host=$FRONTEND_IP ansible_user=ec2-user
 
-echo "inventory.ini generated successfully."
+[backend]
+u21.local ansible_host=$BACKEND_IP ansible_user=ubuntu
+EOF
